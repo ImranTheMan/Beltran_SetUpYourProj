6084adae0d3236b884569125d470bb9ae1c93bac
ImranTheMan <ijason2002@gmail.com>
2023-09-04 23:08:00 +0800

    Upload

---
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            DateTime currentDate = DateTime.Now;
+
+            // Define an array of month names
+            string[] monthNames = {
+            "January", "February", "March", "April", "May", "June", "July",
+            "August", "September", "October", "November", "December"
+        };
+
+            // Format the date string
+            string formattedDate = currentDate.ToString("dddd, d, MMMM");
+
+            // Replace the month placeholder with the actual month name
+            formattedDate = formattedDate.Replace("MMMM", monthNames[currentDate.Month - 1]);
+
+            ViewBag.CurrentDate = formattedDate;
+
+            return View();
+        }
+    }
+}
