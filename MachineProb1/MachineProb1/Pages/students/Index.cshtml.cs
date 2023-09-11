f576db8ecb3dd86951ba39a0251be27e0d4b650c
ImranTheMan <ijason2002@gmail.com>
2023-09-11 20:12:25 +0800

    update

---
+++ MachineProb1/MachineProb1/Pages/students/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MachineProb1.Pages.students
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
